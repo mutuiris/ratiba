9c28d523041f7d80bf9a635fa21e17aeb9c407a0
mutuiris <muneneaddy@gmail.com>
2026-07-12 22:39:55 +0300

    Add token authentication endpoints to URL configuration

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,11 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/token", TokenObtainPairView.as_view(), name="token-obtain"),
+    path("api/token/refresh", TokenRefreshView.as_view(), name="token-refresh"),
     path("api/", include("clinic.api.urls")),
 ]
