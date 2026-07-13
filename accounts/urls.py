7688e00d732d06c731b4eb55ad51223853f21858
mutuiris <muneneaddy@gmail.com>
2026-07-13 14:57:26 +0300

    Add URL routes for authentication including signup, login, and logout

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+"""URL routes for authentication"""
+
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
+from accounts import views
+
+urlpatterns = [
+    path("signup/", views.signup, name="signup"),
+    path("login/", auth_views.LoginView.as_view(template_name="clinic/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+]
